a7325b4f2bac048342ca941356713d4992b4d99d
Ubuntu <ubuntu@ip-172-31-38-148.us-east-2.compute.internal>
2024-07-05 21:17:47 +0000

    move configuration out of build
    Doesn't work on subsequent re-runs
    
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-38-148.us-east-2.compute.internal>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,5 +5,16 @@ set -eou pipefail
 (
   set -x
   pushd /home/runner/actions-runner
+
+  ./config.sh \
+    --unattended \
+    --replace \
+    --ephemeral \
+    --name "${RUNNER_NAME}" \
+    --work "_work" \
+    --labels "${RUNNER_LABEL}" \
+    --url "${RUNNER_REPO_URL}" \
+    --token "${RUNNER_TOKEN}"
+
   ./run.sh
 )
